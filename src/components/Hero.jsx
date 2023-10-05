063adfa7fdddfbcdc7104c24e861ee295d076924
AbdullahiAden <Abbe6152@gmail.com>
2023-10-05 17:13:59 +0200

    minor h1 change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
         </button>
       </nav>
       <h1 className="head_text">
-        Summarize Articles with
+        Summarize & Translate Articles with
         <br className="max-md:hidden" />
         <span className="orange_gradient">OpenAI GPT-4</span>
       </h1>
